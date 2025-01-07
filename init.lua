a7786ac808d1cc329a12adf046ae5baba47f6e23
Gynt <Gynt@users.noreply.github.com>
2025-01-07 14:17:14 +0100

    simplify version sharing

--- init.lua
+++ init.lua
@@ -1,37 +1,27 @@
 
 
-local protocol
+local protocol = {
 
--- This protocol has parameter size 8 (two ints). Parameter size is constant (maybe can be made dynamic, max is 1260)
-protocol = {
-
-  -- This is called when a user initiates the protocol (with a queueProtocol(protocolNumber) call)
-  -- The protocol designer is responsible for storing the to-be-send data somewhere in memory
   scheduleForSend = function(self, meta)
     
-    -- For example, assume you want to implement that sleep can be toggled per building (instead of per building type)
-    -- meta.parameters:writeInteger(core.readInteger(PLACE_WHERE_BUILDING_ID_IS_STORED))
-    -- meta.parameters:writeInteger(core.readInteger(PLACE_WHERE_BUILDING_SLEEP_STATE_IS_STORED))
-
   end,
 
-  -- This is called when a command is received in multiplayer.
-  -- There is nothing to be done here actually. But hey, the programmers wrote it anyway...
   scheduleAfterReceive = function(self, meta)
     
   end,
 
-  -- This is called when the command should be executed (committed to the game state)
-  -- This is an example Delayed protocol, meaning the execute() commits something to game state that should be synchronised across machines (executed simultaenously across machines)
-  -- This is synchronised in game match time across machines in case of a Delayed protocol. 
-  -- In case of a Direct protocol, which is meant for lobby, chat, and UI operations (meta communication),
-  -- execute is executed immediately on each machine
   execute = function(self, meta)
     -- todo, insert config hash...
     log(1, "Computing config hash...")
-    local hash = "DEADBEEFDEADBEEFDEADBEEFDEADBEEF"
+    
+    -- local hash = require("data.version").digest
+    local cAddr = data.version.custom_menu_version_space
+    -- TODO: this string contains %d, make it format to we include the .41
+    local v = string.format(core.readString(cAddr), data.version.game_version.minor)
 
-    modules.chat:sendChatMessage(hash, {1, 2, 3, 4, 5, 6, 7, 8})
+    -- This is also an option
+    -- modules.chat:fireChatEvent(hash, 1)
+    modules.chat:sendChatMessage(v, {1, 2, 3, 4, 5, 6, 7, 8})
   end,
 
 }
@@ -42,18 +32,20 @@ local checkIsHost = function() return core.readInteger(IS_HOST_ADDRESS) ~= 0 end
 
 local namespace = {
   enable = function(self, config)
-    local protocolNumber = modules.protocol:registerProtocol("config-similarity-protocol", "config-similarity-protocol", "DIRECT", 32, protocol)
-    modules.chatcommands:registerChatCommand("checkconfigsimilarity", function(command)
+    local protocolNumber = modules.protocol:registerCustomProtocol("config-similarity-protocol", "config-similarity-protocol", "IMMEDIATE", 0, protocol)
+    local cb = function(args, command)
       -- If you want to set up your data gathering before sending the command, do it here and now just like the game does
 
       if not checkIsHost() then
         return false, "This command is only available as a host"
       end
 
-      modules.protocol:queueProtocol(protocolNumber)
+      modules.protocol:invokeProtocol(protocolNumber)
 
       return false
-    end)
+    end
+    modules.chatcommands:registerChatCommand("checkconfigsimilarity", cb)
+    modules.chatcommands:registerChatCommand("check", cb)
   end,
   disable = function(self, config)
   end,
